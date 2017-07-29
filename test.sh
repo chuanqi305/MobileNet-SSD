c80df8b12e3bd1d7073e192f458a7530c568fb90
chuanqi305 <yaocq@lenovo-ar.com>
2017-07-29 08:54:26 -0700

    automatically use the latest snapshot to test

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#latest=snapshot/mobilenet_iter_73000.caffemodel
+latest=$(ls -t snapshot/*.caffemodel | head -n 1)
+if test -z $latest; then
+	exit 1
+fi
 ../../build/tools/caffe train -solver="solver_test.prototxt" \
---weights=snapshot/mobilenet_iter_73000.caffemodel \
+--weights=$latest \
 -gpu 0
